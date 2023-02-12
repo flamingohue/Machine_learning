3eee39049823a60284fda763a81a55d7806ba97e
flamingohue <fangao_gf@outlook.com>
2023-02-12 18:53:10 -0500

    update linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,60 +1,90 @@
 import numpy as np
+import pandas as pd
 
 class linearRegression():
     def __init__(self,dim):
         self.dim = dim
         self.weight = np.random.normal(0, 1, self.dim[1]+1)
 
-    @staticmethod
     def predict(self, x):
         x = np.hstack((x, np.ones((x.shape[0],1))))
-        y_hat = np.dot(x,weight)
+        y_hat = np.dot(x,self.weight)
         return y_hat
     
+    def check_g(self,x):
+        x = np.hstack((x, np.ones((x.shape[0],1))))
+        y_hat = np.dot(x,self.weight)
+        cost = np.sum(np.dot(y_hat - y,y_hat - y))/x.shape[0]
+
+        gradient = []
+        for i in range(len(self.weight)-1):
+            weight_update = np.add(self.weight, [-0.01 if j==i else 0 for j in range(x.shape[1])])
+            y_hat_check = np.dot(x,weight_update)
+            cost_check = np.sum(np.dot(y_hat_check - y,y_hat_check - y))/x.shape[0]
+            gradient.append(((cost-cost_check)/0.01))
+        print('gradient', (cost-cost_check)/0.01)
+
+
+
+    
     def cost_function(self, x, y):
-        y_hat = predict(x)
-        cost = np.sum(np.dot(y_hat - y,y_hat - y))
+        y_hat = self.predict(x)
+        cost = np.sum(np.dot(y_hat - y,y_hat - y))/x.shape[0]
         return cost
     
     def gradientDescend(self, x, y):
         x = np.hstack((x, np.ones((x.shape[0],1))))
-        gradient = 2/len(x)*np.sum(np.dot(y,x)+x@x@weight)
+        gradient = 2 / x.shape[0] * (np.dot(y.T,x) + x.T @ x @ self.weight)
         return gradient
     
 
 
 class SGD():
-    def __init__(self,model,  a,iteration, tol):
+    def __init__(self,model,a, tol):
         self.model = model
         self.a = a
         self.tol = tol
-        self.iteration = iteration
-        self.i = 0
     
     def weight_update(self, gradient):
-        if self.i < self.iteration or gradient > self.tol:
+        if np.linalg.norm(gradient) > self.tol:
             self.model.weight -= self.a * gradient
-            self.i += 1
             return True
         else:
             return False
 
 
 if __name__=='__main__':
-    lr = linearRegression()
-    optimzier = SGD(lr, 0.01, 1000, 0.00001)
-    x = np.random.randn(20,1000)
-    y = [np.random.randint(0, 1) for i in range(1000)]
-    epoch = 5
+    np.random.seed(1)
+    x1 = np.vstack((np.random.normal(4,2,500),np.random.normal(4, 0.5, 500))).reshape(500,2)
+    y1 = np.array([0] * 500).reshape(500,1)
+    x1_update = np.hstack((x1,y1))
+    x2 = np.vstack((np.random.normal(0,2,500),np.random.normal(0, 0.5, 500))).reshape(500,2)
+    y2 = np.array([1] * 500).reshape(500,1)
+    x2_update = np.hstack((x2,y2))
+    dataset = np.concatenate((x1_update,x2_update))
+    x = dataset[:,:-1]
+    y = dataset[:,-1]
 
-    from sklearn.model_selection import KFold
-    KFold(n_splits=2, random_state=None, shuffle=True)
+    epoch = 100
+    lr = linearRegression(x.shape)
+    optimzier = SGD(lr, 0.0001, 0.0000000001)
 
+    stop = True
     for i in range(epoch):
-        for j, (train_index, test_index) in enumerate(kf.split(x)):
-            gradient = lr.gradientDescend(x[train_index], y[train_index])
-            optimzier.weight_update(gradient)
-        
+        arr = np.arange(1000)
+        np.random.shuffle(arr)
+        arr = arr.reshape((10,100))
+
+        for choice in arr:
+            x_train = dataset[choice, :-1]
+            y_train = dataset[choice, -1]
+            gradient = lr.gradientDescend(x_train, y_train)
+            stop = optimzier.weight_update(gradient)
+            if not stop:
+                break
+        if not stop:
+            break    
+        print(lr.cost_function(x,y))
     
         
     
