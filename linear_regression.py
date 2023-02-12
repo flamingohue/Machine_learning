facbf17d8e35bc81cdc4b7efe557be90c702311a
flamingohue <fangao_gf@outlook.com>
2023-02-12 18:57:17 -0500

    update linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -2,37 +2,34 @@ import numpy as np
 import pandas as pd
 
 class linearRegression():
+    '''
+    linear regression model
+    '''
     def __init__(self,dim):
         self.dim = dim
         self.weight = np.random.normal(0, 1, self.dim[1]+1)
 
     def predict(self, x):
+        '''
+        predict the y
+        '''
         x = np.hstack((x, np.ones((x.shape[0],1))))
         y_hat = np.dot(x,self.weight)
         return y_hat
-    
-    def check_g(self,x):
-        x = np.hstack((x, np.ones((x.shape[0],1))))
-        y_hat = np.dot(x,self.weight)
-        cost = np.sum(np.dot(y_hat - y,y_hat - y))/x.shape[0]
-
-        gradient = []
-        for i in range(len(self.weight)-1):
-            weight_update = np.add(self.weight, [-0.01 if j==i else 0 for j in range(x.shape[1])])
-            y_hat_check = np.dot(x,weight_update)
-            cost_check = np.sum(np.dot(y_hat_check - y,y_hat_check - y))/x.shape[0]
-            gradient.append(((cost-cost_check)/0.01))
-        print('gradient', (cost-cost_check)/0.01)
-
-
 
     
     def cost_function(self, x, y):
+        ''' 
+        Calculate the cost function
+        '''
         y_hat = self.predict(x)
         cost = np.sum(np.dot(y_hat - y,y_hat - y))/x.shape[0]
         return cost
     
     def gradientDescend(self, x, y):
+        '''
+        calculate the gradient for the linear regression
+        '''
         x = np.hstack((x, np.ones((x.shape[0],1))))
         gradient = 2 / x.shape[0] * (np.dot(y.T,x) + x.T @ x @ self.weight)
         return gradient
@@ -40,12 +37,19 @@ class linearRegression():
 
 
 class SGD():
+    '''
+    Stochastic gradient descent optimizer
+    '''
     def __init__(self,model,a, tol):
         self.model = model
         self.a = a
         self.tol = tol
     
     def weight_update(self, gradient):
+        """
+        creating early stopping criteria and update the weight 
+        """
+
         if np.linalg.norm(gradient) > self.tol:
             self.model.weight -= self.a * gradient
             return True
@@ -55,6 +59,7 @@ class SGD():
 
 if __name__=='__main__':
     np.random.seed(1)
+    # simulate the datasets
     x1 = np.vstack((np.random.normal(4,2,500),np.random.normal(4, 0.5, 500))).reshape(500,2)
     y1 = np.array([0] * 500).reshape(500,1)
     x1_update = np.hstack((x1,y1))
@@ -65,6 +70,7 @@ if __name__=='__main__':
     x = dataset[:,:-1]
     y = dataset[:,-1]
 
+    # setting the hyperparameter and running for 100 times
     epoch = 100
     lr = linearRegression(x.shape)
     optimzier = SGD(lr, 0.0001, 0.0000000001)
